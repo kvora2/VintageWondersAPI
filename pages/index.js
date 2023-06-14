1ab69ec2c8a388d30e5a29b4be124a33bb7194af
kvora2 <kelvinvora21@gmail.com>
2023-06-13 21:03:51 -0400

    link added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 /*********************************************************************************
-*  BTI425 – Assignment 4
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: Kelvin Vora Student ID: 157616210 Date: 10th March 2023
-*
+Vercel Link :-
+https://vintage-wonders-on2umtydp-kvora2.vercel.app/
 ********************************************************************************/
 
 import { Row, Col, Image } from 'react-bootstrap';
